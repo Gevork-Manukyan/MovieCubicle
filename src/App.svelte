3f3cced4a23c2129d5424e28054e646a28e9cfeb
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-21 23:53:10 -0700

    adds navbar and some colors

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,24 @@
 <script>
-	export let name;
+	import NavBar from "./NavBar.svelte";
 </script>
 
+
+<header>
+	<h1 id="title">Movie Gang</h1>
+	<NavBar />
+</header>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
 </main>
+<footer>
+
+</footer>
+
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	#title {
+		margin: 0;
+		padding: 17px 0px 17px 20px;
+		color: #fff;
 	}
 </style>
