7a19023a1754d5149b0eddcaff2e607e5da71d84
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-28 14:19:10 -0700

    got fetch working with moviedb api

---
+++ src/services/Api.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    async function request() {
+        try {
+			const res = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${MOVIE_SECRET_KEY}`)
+            let jsonRes = await res.json()
+            return jsonRes;
+		} catch (err) {
+			console.log(err);
+		}
+    }
+
+    export async function baseRequest() {
+        return await request();
+    }
+</script>
