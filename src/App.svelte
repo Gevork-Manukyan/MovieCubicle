23d1f2544d42cfaed198ea41a16d0ce6e199db00
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-28 14:40:21 -0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,32 @@
 <script>
 	import NavBar from "./NavBar.svelte";
+	import { getAllDailyTrending, getCustomTrending } from "./services/Api.svelte"
+
+	// TODO: Remove below test code
+	async function createParseUser() {
+		// Creates a new Parse "User" object, which is created by default in your Parse app
+		let user = new Parse.User();
+		// Set the input values to the new "User" object
+		user.set("username", "steve");
+		user.set("email", "Test1@test.com");
+		user.set("password", "123456");
+		try {
+			// Call the save method, which returns the saved object if successful
+			user = await user.save();
+			if (user !== null) {
+			// Notify the success by getting the attributes from the "User" object, by using the get method (the id attribute needs to be accessed directly, though)
+			alert(
+				`New object created with success! ObjectId: ${
+				user.id
+				}, ${user.get("username")}`
+			);
+			}
+		} catch (error) {
+			alert(`Error: ${error.message}`);
+		}
+	}
+	// createParseUser()
+
 </script>
 
 
