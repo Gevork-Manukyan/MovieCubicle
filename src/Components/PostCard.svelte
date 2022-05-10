8886c68bd557200a6b05634a90770ccc919d1a2d
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-05-10 12:42:57 -0700

    extract PostCard component

---
+++ src/Components/PostCard.svelte
@@ -0,0 +1,46 @@
+<script>
+
+</script>
+
+
+<div class="postcard">
+    <a href="#">
+        <!-- image of the movie goes here -->
+        <img src="" alt="">
+        <!-- description of the movie goes here -->
+        <h2 class="Title">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae ducimus minima, placeat excepturi quae velit corporis quaerat natus perferendis ratione fugiat. Laudantium itaque consectetur ullam iusto, cum quia repellendus officiis?</h2>
+    </a>
+</div>
+
+
+<style>
+    div{
+        color: aliceblue;
+    }
+
+    .postcard{
+        width: 200px;
+        height: 300px;
+        background: rgb(0, 0, 0);
+        border: 2px solid red;
+        border-radius: 10%;
+    }
+
+    .postcard a{
+        text-decoration: none;
+        
+    }
+    .postcard a img{
+        width:100%;
+        height: 280px;
+
+    }
+
+    .Title{
+        height: 80px;
+        font-size: 14px;
+        margin: 4px 0;
+        padding: 0 5px;
+        color: white;
+    }
+</style>
