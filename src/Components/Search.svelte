a0dc718d3676d702667112c92d2277a5a5557b00
frankthe-tank <cejafrank96@gmail.com>
2022-05-13 17:29:47 -0700

    changes

--- src/Components/Search.svelte
+++ src/Components/Search.svelte
@@ -1,12 +1,15 @@
 <script>
+    import searchStore from "../Stores/SearchStore.js";
+    import movieDataStore from "../Stores/MovieDataStore.js";
     import { searchMovie } from "../services/Api.svelte";
-    import PostCard from "./PostCard.svelte"
-    import NavBar from "./NavBar.svelte"
+    import PostCard from "./PostCard.svelte";
+    import NavBar from "./NavBar.svelte";
     
     export let params
     let movies=[];
 
     const inputValue=params.inputvalue
+
     async function fetchMovies(inputValue){
         let res=await searchMovie(inputValue);
         movies=res.results;
@@ -14,6 +17,9 @@
 
     fetchMovies(inputValue);
     $: console.log("these are my movies: " +movies);
+
+    movieDataStore.set(movies)
+    searchStore.set(movies)
 </script>
 <nav>
     <NavBar />    
